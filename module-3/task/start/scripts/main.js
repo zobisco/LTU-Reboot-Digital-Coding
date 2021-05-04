0a49090aed74c267944aba7369ee44e8d2447400
lukeashtonjohnson <lukeashtonjohnson@gmail.com>
2021-05-04 11:22:25 +0100

    Update main.js
    Add single line and multiple line comments to the head  of main.js
    
    Add 5 variables and console logged the result

---
+++ module-3/task/start/scripts/main.js
@@ -0,0 +1,17 @@
+// This is a single line comment
+
+/*
+this is
+a
+multiple line
+comment
+*/
+
+const productName = 'Baked beans'; // string
+let price = 0.89; // number - float
+let quantity = 2; // number Integer
+let inStock = true; // Boolean
+let discountAmount = 0.20; // number - float
+
+// console.log( productName, price );
+
