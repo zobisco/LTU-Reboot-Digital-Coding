6823fe59d9dae022b84d05150d8770c116c33690
lukeashtonjohnson <lukeashtonjohnson@gmail.com>
2021-05-05 11:17:59 +0100

    Update main.js
    create function and converted to arrow function, if and else statement, basic arithemtic

--- module-3/task/start/scripts/main.js
+++ module-3/task/start/scripts/main.js
@@ -15,3 +15,37 @@ let discountAmount = 0.20; // number - float
 
 // console.log( productName, price );
 
+function showProductName() {
+  console.log(productName);
+}
+
+//showProductName();
+
+function totalPrice( productPrice,productQuantity ) {
+
+ console.log(productPrice * productQuantity) ;
+
+}
+
+// totalPrice(price, quantity);
+
+// function checkAvailability( productInStock ) {
+//   console.log( productInStock );
+// }
+
+let checkAvailability = productInStock => console.log( productInStock );
+
+// checkAvailability( inStock );
+
+function productDiscount() {
+
+  if ( quantity > 1 ) { // if this is true
+    let newPrice = (price - discountAmount * quantity);
+    console.log( newPrice );
+  } else { // if this is false
+    console.log( price * quantity );
+  }
+
+}
+
+// productDiscount();
