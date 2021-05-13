bcc053f7eb1b1b01e55e35736ddbe1aee9f4e9f3
lukeashtonjohnson <lukeashtonjohnson@gmail.com>
2021-05-13 12:21:42 +0100

    Update filter.js
    demo how to refactor code in filter.js

--- module-3/final_project/start/scripts/filter.js
+++ module-3/final_project/start/scripts/filter.js
@@ -2,8 +2,26 @@ const buttons = document.querySelectorAll('button');
 
 let allImages = document.querySelectorAll('.image');
 
+const toggleImages = ( dataClass ) => {
+  // Ternary 
+  dataClass === "all" 
+    ? allImages.forEach( image => image.style.display = 'flex')
+    : allImages.forEach( image => {
+        image.dataset.class === dataClass
+          ? image.style.display = 'flex'
+          : image.style.display = 'none';
+    })
 
-function toggleImages( dataClass ) {
+} // end of toggleImages function
 
+const toggleActiveClass = ( active ) => {
+  buttons.forEach( button => button.classList.remove("active")); // loop over the buttons and remove the active class from all the buttons
+  active.classList.add("active");
 }
 
+buttons.forEach( button => {
+  button.addEventListener('click', ()=>{
+    toggleImages( button.dataset.class );
+    toggleActiveClass( button );
+  })
+});
